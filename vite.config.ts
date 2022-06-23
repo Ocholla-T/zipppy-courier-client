a3e50ed7fc90ea36086dfdf2e970eab0c08f6e79
Ocholla-T <tonnyocholla8@gmail.com>
2022-06-23 15:53:11 +0300

    ADD: path aliases for vaious routes in application

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
       '@styles': path.resolve(__dirname, './src/assets/styles'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@pages': path.resolve(__dirname, './src/components/pages'),
     },
   },
 })
