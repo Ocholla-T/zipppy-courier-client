40a69e1505ef757414c4002662465021473af139
Ocholla-T <tonnyocholla8@gmail.com>
2022-06-30 09:37:16 +0300

    ADD: styling and template for alert modal

---
+++ src/components/ui/Alert/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  isSuccess: boolean
+}>()
+
+defineEmits(['click'])
+</script>
+
+<template>
+  <div class="alert flex flex-ai-c" :class="{ success: isSuccess }">
+    <p class="alert__info">{{ text.replace(/['"]+/g, '') }}</p>
+    <img
+      class="alert__icon"
+      src="../../../assets/images/close.svg"
+      alt="close alert"
+      @click="$emit('click')"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use './styles';
+</style>
